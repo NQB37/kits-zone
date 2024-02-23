86c5bbae823e219baf68c7ff96767c9137c41424
nquocbao37 <nquocbao37@gmail.com>
2024-02-23 10:54:29 +0100

    add login page

---
+++ src/pages/Auth/SignIn.jsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import Login from '../../components/Login/Login';
+const SignIn = () => {
+    return (
+        <div>
+            <Header />
+            <Login />
+            <Footer />
+        </div>
+    );
+};
+
+export default SignIn;
